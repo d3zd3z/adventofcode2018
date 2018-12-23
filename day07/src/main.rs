77e4d7f1366075b817cfdf980dc3291d53c9a496
David Brown <david.brown@linaro.org>
2018-12-23 10:17:16 -0700

    day07: Solve second part
    Use a BinaryHeap as a priority queue and implement the problem
    description.

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,6 +1,10 @@
 use regex::Regex;
 use std::{
-    collections::BTreeSet,
+    cmp::Ordering,
+    collections::{
+        BinaryHeap,
+        BTreeSet,
+    },
     fs::File,
     io::{BufRead, BufReader},
     mem,
@@ -10,12 +14,41 @@ use std::{
 type Result<T> = result::Result<T, failure::Error>;
 
 fn main() -> Result<()> {
-    let mut depends = get_input()?;
+    let depends = get_input()?;
 
     let mut t1 = Tracker::new(depends.clone());
     t1.solve1();
 
+    let mut t2 = Tracker::new(depends);
+    t2.solve2(5);
+
     Ok(())
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+struct Worker {
+    /// Time when this worker is done.
+    finish: usize,
+
+    /// What this working is working on.
+    work: char,
+}
+
+// Implement a reversed Ord as ordered by decreasing time.  (binary_heap is
+// a max_queue by default).  Ties are won with ordering (lowest first as
+// well) of the working character.  This is needed so that PartialEq is
+// consistent with ordering.
+impl Ord for Worker {
+    fn cmp(&self, other: &Worker) -> Ordering {
+        other.finish.cmp(&self.finish)
+            .then_with(|| other.work.cmp(&self.work))
+    }
+}
+
+impl PartialOrd for Worker {
+    fn partial_cmp(&self, other: &Worker) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 struct Tracker {
@@ -40,6 +73,38 @@ impl Tracker {
         }
 
         println!("Result1: {:?}", result);
+    }
+
+    fn solve2(&mut self, nworkers: usize) {
+        let mut time = 0;
+        let mut workers = BinaryHeap::new();
+
+        loop {
+            // Fill up any missing work.
+            while workers.len() < nworkers {
+                if let Some(best) = self.best() {
+                    workers.push(Worker{
+                        finish: time + (best as usize) - 4,  // A is 65.
+                        work: best,
+                    });
+                    // Remove from todo list early.
+                    self.todo.remove(&best);
+                } else {
+                    break;
+                }
+            }
+
+            // Take the best work.
+            if let Some(dw) = workers.pop() {
+                self.mark_done(dw.work);
+                time = dw.finish;
+            } else {
+                break;
+            }
+        }
+
+        // The result is the timer when we are done.
+        println!("Result2: {}", time);
     }
 
     /// Return the best possible move, with the given dependencies.
